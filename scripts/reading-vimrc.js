67a5bb34ba6a95d06c4924028b44e4426c9884b4
thinca <thinca@gmail.com>
2016-11-06 17:14:59 +0900

    Move ReadingVimrc class to lib

--- scripts/reading-vimrc.js
+++ scripts/reading-vimrc.js
@@ -38,100 +38,10 @@ const path = require("path");
 const YAML = require("js-yaml");
 const printf = require("printf");
 
+const ReadingVimrc = require("../lib/reading_vimrc");
+
 const ROOM_NAME = process.env.HUBOT_READING_VIMRC_ROOM_NAME || "vim-jp/reading-vimrc";
 const ADMIN_USERS = (process.env.HUBOT_READING_VIMRC_ADMIN_USERS || "").split(/,/);
-
-class ReadingVimrc {
-  constructor() {
-    this.id = 0;
-    this.startLink = "";
-    this.messages = [];
-    this.vimrcs = [];
-    this.isRunning = false;
-    this.vimrcContents = new Map();
-    this.vimrcLastname = new Map();
-  }
-
-  get status() {
-    return this.isRunning ? "started" : "stopped";
-  }
-
-  get members() {
-    return [...new Set(this.messages.map((mes) => mes.user.login)).values()];
-  }
-
-  start(id, link, vimrcs, part) {
-    this.id = id;
-    this.startLink = link;
-    this.vimrcs = vimrcs;
-    this.messages = [];
-    this.isRunning = true;
-    this.part = part;
-    this.clearVimrcs();
-  }
-
-  stop() {
-    this.isRunning = false;
-  }
-
-  reset() {
-    this.restore_cache = this.messages;
-    this.messages = [];
-  }
-
-  restore() {
-    [this.restore_cache, this.messages] = [this.messages, this.restore_cache];
-  }
-
-  add(message) {
-    if (!this.isRunning) {
-      return;
-    }
-    this.messages.push(message);
-  }
-
-  setVimrcContent(name, content) {
-    this.vimrcContents.set(name, content.split(/\r?\n/));
-  }
-
-  getVimrcFile(namePat, username) {
-    const names = [...this.vimrcContents.keys()];
-    let name;
-    if (namePat) {
-      name = [
-        `^${namePat}$`,
-        `/${namePat}(?:\..*)?$`,
-        namePat
-      ].reduce((k, pat) => {
-        if (!k) {
-          const reg = new RegExp(pat, "i");
-          k = names.find((k) => reg.test(k));
-        }
-        return k;
-      }, null);
-    } else if (this.vimrcLastname.has(username)) {
-      name = this.vimrcLastname.get(username);
-    } else {
-      name = names[0];
-    }
-    if (!this.vimrcContents.has(name)) {
-      return [];
-    }
-    if (username) {
-      this.vimrcLastname.set(username, name);
-    }
-    return [name, this.vimrcContents.get(name)];
-  }
-
-  getVimrcLines(content, startLine, endLine = startLine) {
-    return content.slice(startLine - 1, endLine);
-  }
-
-  clearVimrcs() {
-    this.vimrcContents.clear();
-    this.vimrcLastname.clear();
-  }
-}
 
 const helpMessage = `vimrc読書会で発言した人を集計するための bot です
 
